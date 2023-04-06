e2814e932a8ab53e96105e879228d1d13168f64e
Henry Limm <henry.limm@motivps.com>
2023-04-06 12:27:57 -0700

    Add hop.nvim with keymaps.

--- lua/specs/vimfunc.lua
+++ lua/specs/vimfunc.lua
@@ -162,5 +162,18 @@ return {
 			vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
 		end
 	},
+	{
+		'phaazon/hop.nvim',
+		event = 'VeryLazy',
+		config = function()
+			local hop = require('hop')
+			hop.setup({uppercase_labels = true,})
+			local directions = require('hop.hint').HintDirection
+			vim.keymap.set('', 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end)
+			vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end)
+			vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end)
+			vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end)
+			vim.keymap.set('', 'ff', function() hop.hint_words() end)
+		end,
 	},
 }
