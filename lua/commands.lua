8caf817ad1afa0ab8bbf39b0ec95ca8057e52b73
Henry Limm <henry.limm@motivps.com>
2023-04-21 12:29:51 -0700

    Refactor user shebang and python script commands.

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,33 +1,41 @@
 -- miscellaneous collection of custom commands
 local api = vim.api
 
-api.nvim_create_user_command(
-	'PyScript',
-	function(args)
-		local index = api.nvim_win_get_cursor(0)[1] - 1
-		api.nvim_buf_set_lines(
-			0,
-			index,
-			index,
-			false,
-			{
-				'#!/usr/bin/env python3',
-				'', '',
-				'def main(*args, **kwargs):',
-				'    return 0',
-				'', '',
-				'def mainargs(argv=None):',
-				'    import argparse',
-				'    pser = argparse.ArgumentParser()',
-				'',
-				'    args = pser.parse_args(argv)',
-				'    return vars(args)',
-				'', '',
-				'if __name__ == "__main__":',
-				'    import sys',
-				'    sys.exit(main(**mainargs()))'
-			}
-		)
-	end,
-	{}
-)
+local shebang = '#!/usr/bin/env '
+
+function write_shebang (exargs)
+	local target = exargs.args
+	if target == '' then target = 'python3' end
+	api.nvim_buf_set_lines(0, 0, 0, false, {shebang .. target})
+end
+api.nvim_create_user_command('SheBang', write_shebang, {nargs='?'} )
+
+function pyscript(exargs)
+	-- local index = api.nvim_win_get_cursor(0)[1] - 1
+	local main = {'def main(*args, **kwargs):', '    return 0'}
+	local mainargs = {
+		'def mainargs(argv=None):',
+		'    import argparse',
+		'    pser = argparse.ArgumentParser()',
+		'',
+		'    args = pser.parse_args(argv)',
+		'    return vars(args)',
+	}
+	local namemain = {
+		'if __name__ == "__main__":',
+		'    import sys',
+		'    sys.exit(main(**mainargs()))',
+	}
+	for i, chunk in ipairs({
+		{shebang .. 'python3'},
+		main,
+		{'', ''},
+		mainargs,
+		{'', ''},
+		namemain,
+	}) do
+		api.nvim_buf_set_lines( 0, -1, -1, false, chunk )
+	end
+end
+
+api.nvim_create_user_command( 'PyScript', pyscript, {} )
