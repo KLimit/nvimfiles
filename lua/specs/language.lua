1714127a9692c2fdc8b8dff42c99e7075ee3d0b5
Henry Limm <henry.limm@motivps.com>
2023-04-25 15:52:16 -0700

    Split treesitter plugins from language plugins.

--- lua/specs/language.lua
+++ lua/specs/language.lua
@@ -45,46 +45,4 @@ return {
 			vim.g.fixjson_fix_on_save = 0
 		end,
 	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		lazy = false,
-		build = function()
-			require('nvim-treesitter.install').update({
-				with_sync = true
-			})
-		end,
-		config = function()
-			-- might be hacky, but specify use of zig compiler
-			require'nvim-treesitter.install'.compilers = {'zig'}
-			require'nvim-treesitter.configs'.setup {
-				ensure_installed = {
-					'c',
-					'gitcommit',
-					'json',
-					'lua',
-					'perl',
-					'python',
-					'query',
-					'rust',
-					'vim',
-					'vimdoc',
-				},
-				highlight = { enable = false },
-				query_linter = { enable = true, use_virtual_text = true, lint_events = {"BufWrite", "CursorHold"}},
-			}
-		end,
-	},
-	{
-		'nvim-treesitter/nvim-treesitter-context',
-		lazy = false,
-		event = 'VeryLazy',
-		config = function()
-			require('treesitter-context').setup()
-		end
-	},
-	{
-		'nvim-treesitter/playground',
-		enabled=true,
-		cmd = 'TSPlaygroundToggle',
-	}
 }
