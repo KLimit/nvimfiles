d80e174eaf13686315e5dd12f9a79c5fb1801e5c
Henry Limm <henry.limm@motivps.com>
2023-04-25 13:28:10 -0700

    Add less-like mappings for help filetypes.

---
+++ ftplugin/help.lua
@@ -0,0 +1,9 @@
+-- make viewing help files more pager-like
+local map = require('maps').bmap
+local opt = vim.opt_local
+map('n', 'j', '<C-E>')
+map('n', 'k', '<C-Y>')
+map('n', 'd', '<C-D>', {nowait=true})
+map('n', 'u', '<C-U>')
+map('n', '<C-K>', 'k', {noremap=true})
+opt.cursorline = false
