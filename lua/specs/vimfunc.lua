feaeef2a1b45aba23ac13470f2ff07ff165879d7
Henry Limm <henry.limm@motivps.com>
2023-04-25 15:52:35 -0700

    Disable some keymaps from hop.nvim.

--- lua/specs/vimfunc.lua
+++ lua/specs/vimfunc.lua
@@ -142,10 +142,10 @@ return {
 			local hop = require('hop')
 			hop.setup({uppercase_labels = true,})
 			local directions = require('hop.hint').HintDirection
-			vim.keymap.set('', 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end)
-			vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end)
-			vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end)
-			vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end)
+			-- vim.keymap.set('', 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end)
+			-- vim.keymap.set('', 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end)
+			-- vim.keymap.set('', 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end)
+			-- vim.keymap.set('', 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end)
 			vim.keymap.set('', 'ff', function() hop.hint_words() end)
 		end,
 	},
