f347bda5d077da7e112850c8f14571a1bad471bd
Henry Limm <henry.limm@motivps.com>
2023-04-28 13:54:37 -0700

    Make ftplugin for git commits that fold untracked files.

---
+++ ftplugin/gitcommit.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt_local
+-- need to load syntax first; doesn't seem to be affected by putting "syntax on"
+-- above "filetype plugin on" in init.vim
+vim.cmd('syntax on')
+opt.foldmethod = 'syntax'
+opt.foldlevel = 1
+opt.foldenable = true
+-- vim.cmd({
+-- 	range = {untrackedline},
+-- 	cmd = 'foldclose',
+-- })
+local function folduntracked()
+	print('in the callback')
+	local untrackedline = vim.fn.search('^# Untracked files:')
+	vim.api.nvim_win_set_cursor(0, {1, 0})
+	vim.cmd({
+		range = {untrackedline},
+		cmd = 'foldclose',
+	})
+end
+folduntracked()
