3bc5459c7b18702d8ad22b265eb8c1f04bdec6de
Henry Limm <henry.limm@motivps.com>
2023-06-13 15:38:22 -0700

    Clean up terminal.lua

--- lua/specs/terminal.lua
+++ lua/specs/terminal.lua
@@ -1,21 +1,17 @@
 -- plugins that add functionality to neovim's terminal
 return {
 	{
-		'samjwill/nvim-unception',
-	},
-	{
 		'numtostr/navigator.nvim',
 		enabled = false,
-		config = function()
-			require('Navigator').setup()
-		end
+		opts = {},
 	},
 	{
-		'boltlessengineer/bufterm.nvim',
-		config = function()
-			require('bufterm').setup({})
-		end,
-		cmd = {'BufTermEnter', 'BufTermPrev', 'BufTermNext', 'Terminal'},
-		-- event = 'VeryLazy',
-	},
+		'numToStr/FTerm.nvim',
+		opts = {
+			cmd = 'pwsh -nologo',
+		},
+		keys = {
+			{'<F12>', function() require("FTerm").toggle() end, mode = {'n', 't'}},
+		},
+	}
 }
