b1fd320b5f3d7d54c378759c70e3517a87fd8ee1
Henry Limm <henry.limm@motivps.com>
2023-07-28 16:21:09 -0700

    Move the pager-like mapping to a lua submodule and add an ftplugin for checkhealth.

---
+++ lua/config/pager.lua
@@ -0,0 +1,11 @@
+-- make viewing help files more pager-like
+-- pager-like keymaps ant other configurations
+local map = require('maps').bmap
+local opt = vim.opt_local
+map('n', 'j', '<C-E>')
+map('n', 'k', '<C-Y>')
+map('n', 'd', '<C-D>', {nowait=true})
+map('n', 'u', '<C-U>')
+map('n', '<C-K>', 'k', {noremap=true})
+map('n', 'q', ':q<CR>', {nowait=true})
+opt.cursorline = false
