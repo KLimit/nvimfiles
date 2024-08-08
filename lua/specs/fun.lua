7fe171c094858b44a371b042eef1833ae41d2846
Henry Limm <henry.limm@motivps.com>
2024-08-08 12:46:22 -0700

    Try to use colorscheme's "color" submodule for card colors.

--- lua/specs/fun.lua
+++ lua/specs/fun.lua
@@ -58,10 +58,33 @@ return {
 		'rktjmp/playtime.nvim',
 		cmd = 'Playtime',
 		config = function()
-			require'playtime'.setup{}
+			require'playtime'.setup{fps=60}
 			local set_hl = vim.api.nvim_set_hl
-			set_hl(0, '@playtime.game.card.diamonds', {link='@playtime.game.card.diamonds.four_colors'})
-			set_hl(0, '@playtime.game.card.clubs', {link='@playtime.game.card.clubs.four_colors'})
+			local function hilink(tolink, target)
+				set_hl(0, tolink, {link=target})
+			end
+			hilink('@playtime.game.card.diamonds', '@playtime.game.card.diamonds.four_colors')
+			hilink('@playtime.game.card.clubs', '@playtime.game.card.clubs.four_colors')
+			-- try to set playtime colors based on color scheme
+			pcall(function()
+				local colors = require(vim.g.colors_name .. '.colors').colors
+				for group, fg in pairs{
+					PlaytimeNormal = colors.fg,
+					-- PlaytimeMuted = colors.fluo,
+					PlaytimeWhite = colors.white,
+					PlaytimeBlack = colors.black,
+					PlaytimeRed = colors.red,
+					PlaytimeGreen = colors.green,
+					PlaytimeYellow = colors.yellow,
+					PlaytimeOrange = colors.orange,
+					PlaytimeBlue = colors.blue,
+					PlaytimeMagenta = colors.purple,
+					PlaytimeCyan = colors.cyan,
+				} do
+					print(group, fg)
+					set_hl(0, group, {fg=fg})
+				end
+			end)
 		end
 	}
 }
